550a96b3957fe4e0c9e78b9b4971545d9c513cef
Vini347 <contato.viniciusdasilva@gmail.com>
2023-05-06 15:31:55 -0300

    retorno tabela processo

--- dataJson/dataJson/indev.py
+++ dataJson/dataJson/indev.py
@@ -1,19 +1,30 @@
-from django.http import JsonResponse
 import psycopg2 as pg
 import json
 
 try:
     connection = pg.connect(user="postgres", password="SDNA@2022", host="localhost", port="5432", database="Eagle2")
     curs = connection.cursor()
-    curs.execute('SELECT processonivelrisco as "Nivel de Risco", COUNT(*) AS "QtdeProcessos" FROM dash_processo GROUP BY processonivelrisco, processodata')
+    curs.execute('SELECT processonivelrisco as "Nivel de Risco", mes as "Mes", ano as "Ano", COUNT(*) AS "QtdeProcessos" FROM dash_processo GROUP BY processonivelrisco, mes, ano')
     # corrigin erro: data nao é um tipo de dado que pode ser armazenado em um JSON
+
     nivel_risco = {
-    'MBA': 'Muito Baixo',
-    'BAI': 'Baixo',
-    'MOD': 'Moderado',
-    'ALT': 'Alto',
-    'MAL': 'Muito Alto'
+    'MBA': 'Muito Baixo', 'BAI': 'Baixo', 'MOD': 'Moderado', 'ALT': 'Alto', 'MAL': 'Muito Alto'
 }
+    dic_date_mounth = {
+        '01' : 'Janeiro',
+        '02' : 'Fevereiro',
+        '03' : 'Marco',
+        '04' : 'Abril',
+        '05' : 'Maio',
+        '06' : 'Junho',
+        '07' : 'Julho',
+        '08' : 'Agosto',
+        '09' : 'Setembro',
+        '10' : 'Outubro',
+        '11' : 'Novembro',
+        '12' : 'Dezembro'
+    }
+
     columns = [col[0] for col in curs.description]
     results = []
     for row in curs.fetchall():
@@ -22,7 +33,7 @@ try:
             if i == 0:
                 result_dict[columns[i]] = nivel_risco[value]
             elif i == 1:
-                result_dict[columns[i]] = value
+                result_dict[columns[i]] = dic_date_mounth
         results.append(result_dict)
 
     curs.close()
