49dae30f3f075635c72d5dc8a379e3a5ebe5d0d2
Vini347 <contato.viniciusdasilva@gmail.com>
2023-05-18 11:12:39 -0300

    novo json

--- dataJson/dataJson/indev.py
+++ dataJson/dataJson/indev.py
@@ -4,32 +4,30 @@ import psycopg2 as pg
 
 connection = pg.connect(user="postgres", password="SDNA@2022", host="localhost", port="5432", database="Eagle2")
 curs = connection.cursor()
-curs.execute ('SELECT tarefaid AS "ID", tarefatitulo AS "Plano", usuario_areaid AS "Area", tarefastatustarefa AS "Status", tarefaprevtermino AS "Prazo" FROM table_tarefa GROUP BY "ID", "Plano", "Area", "Status", "Prazo" ORDER BY "ID", "Plano", "Area", "Status", "Prazo"')
+curs.execute ('SELECT processoid AS "ID", probabilidade AS "Probabilidade", impacto AS "Impacto" FROM matriz_probabilidade_impacto GROUP BY "ID", "Probabilidade", "Impacto" ORDER BY "ID", "Probabilidade", "Impacto"')
 
-dic_status_tarefa = {
-    '1': 'Nao iniciado', 
-    '2': 'Em andamento', 
-    '3': 'Em aprovacao', 
-    '4': 'Concluido', 
-    '5': 'Cancelado'
+dic_probabilidade = {
+    'quase certo': 'Quase certo',
+    'provavel': 'Provavel',
+    'pouco provavel': 'Pouco provavel',
+    'possivel': 'Possivel',
+    'rara': 'Rara'
 }
-dic_area_tarefa = {
-    '1': 'Juridico', 
-    '2': 'Operacional', 
-    '3': 'Financeiro', 
-    '4': 'RH', 
-    '5': 'Comercial',
-    '6': 'Administrativo'
+
+dic_impacto = {
+    'desprezivel': 'Desprezivel',
+    'marginal': 'Marginal',
+    'media': 'Media',
+    'critica': 'Critica',
+    'extrema': 'Extrema'
 }
 
 results = []
 for row in curs.fetchall():
     result_dict = {}
-    result_dict['Status'] = dic_status_tarefa[str(row[3])]
     result_dict['ID'] = row[0]
-    result_dict['Plano'] = row[1]
-    result_dict['Area'] = dic_area_tarefa[str(row[3])]
-    result_dict['Prazo'] = row[4]
+    result_dict['Probabilidade'] = dic_probabilidade[row[1]]
+    result_dict['Impacto'] = dic_impacto[row[2]]
     results.append(result_dict)
 
 curs.close()
--- dataJson/dataJson/views.py
+++ dataJson/dataJson/views.py
@@ -325,6 +325,41 @@ def Tabela_Tarefas(request):
 
     return JsonResponse(results, safe=False)
 
+def Matriz_Probabilidade_Impacto(request):
+    
+    connection = pg.connect(user="postgres", password="SDNA@2022", host="localhost", port="5432", database="Eagle2")
+    curs = connection.cursor()
+    curs.execute ('SELECT processoid AS "ID", probabilidade AS "Probabilidade", impacto AS "Impacto" FROM matriz_probabilidade_impacto GROUP BY "ID", "Probabilidade", "Impacto" ORDER BY "ID", "Probabilidade", "Impacto"')
+
+    dic_probabilidade = {
+        'quase certo': 'Quase certo',
+        'provavel': 'Provavel',
+        'pouco provavel': 'Pouco provavel',
+        'possivel': 'Possivel',
+        'rara': 'Rara'
+    }
+
+    dic_impacto = {
+        'desprezivel': 'Desprezivel',
+        'marginal': 'Marginal',
+        'media': 'Media',
+        'critica': 'Critica',
+        'extrema': 'Extrema'
+    }
+
+    results = []
+    for row in curs.fetchall():
+        result_dict = {}
+        result_dict['ID'] = row[0]
+        result_dict['Probabilidade'] = dic_probabilidade[row[1]]
+        result_dict['Impacto'] = dic_impacto[row[2]]
+        results.append(result_dict)
+
+    curs.close()
+    connection.close()
+
+    return JsonResponse(results, safe=False)
+
 
 # teste local do arquivo JSON>>>>>>>    json_result = json.dumps(final_results, indent=4)
 #                                       with open('resultado.json', 'w') as arquivo:
