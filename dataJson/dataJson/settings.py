50c6206ccb7e91b7486e7d5617cee656cf537da2
Lucas Renan <lucascastelamareei@gmail.com>
2023-05-30 11:26:58 -0300

    ajuste requiriments

--- dataJson/dataJson/settings.py
+++ dataJson/dataJson/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*-ys0h6qx0ypyplpx8^x@y+hj4%(i_9fz-1&qkyxt5+#)$e#9!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 # Application definition
 INSTALLED_APPS = [
     'corsheaders',
